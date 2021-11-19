78b448d7f079f54eb960f375049e60d493efd15a
spa51 <santi.15elspa@outlook.com>
2021-11-19 15:13:13 -0500

    Se añadio pestaña home

--- MyMangoApp/src/app/app-routing.module.ts
+++ MyMangoApp/src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   // Se omite el homo por el moemnto ya que no se diseño
-  // {
-  //   path: 'home',
-  //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  // },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
   {
     path: '',
     redirectTo: 'login',
