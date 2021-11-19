8cbe8f7073497823bb3a19efc606a294b63ff72a
spa51 <santi.15elspa@outlook.com>
2021-11-19 12:29:16 -0500

    Cambios a Login

--- MyMangoApp/src/app/app-routing.module.ts
+++ MyMangoApp/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  // Se omite el homo por el moemnto ya que no se diseño
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  // },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
